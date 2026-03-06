8ebd7afdee29d5d11ddc4ed7b6ef31fc2f19536f
extecoetter <extecoetterrobson@gmail.com>
2026-03-06 20:49:25 -0300

    Update config.js

--- clientes/clientemodelo/config.js
+++ clientes/clientemodelo/config.js
@@ -1,1 +1,8 @@
-
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Cliente Modelo Climatização",
+    whats: "(14) 99999-9999",
+    pix: "14 99999-9999 / Cliente Modelo"
+  },
+  logo: "/climatizaorcamentos/clientes/clientemodelo/logo.png"
+};
