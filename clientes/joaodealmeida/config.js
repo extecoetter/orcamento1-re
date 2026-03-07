6871179cfe023b75d9ae52e15df62d4554f72254
extecoetter <extecoetterrobson@gmail.com>
2026-03-07 18:05:34 -0300

    Add files via upload

---
+++ clientes/joaodealmeida/config.js
@@ -0,0 +1,9 @@
+
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Joao de almeida",
+    whats: "(19) 99705-3503",
+    pix: "19 99705 3503"
+  },
+  logo: "/climatizaorcamentos/clientes/joaodealmeida/logo.png"
+};
